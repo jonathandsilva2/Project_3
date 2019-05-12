cd6a0b98dcf1f776379d846b8e0c84d87ea6d430
jonathandsilva2 <jonathandsilva2@gmail.com>
2019-05-11 20:17:57 -0400

    for loop for filtering first 12 articles complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,39 @@
+
 const gulp = require("gulp");
 
 const terser = require("gulp-terser"),
 rename = require("gulp-rename"),
-browserSync = require("browser-sync").create();
-eslint = require("gulp-eslint");
+browserSync = require("browser-sync").create(),
+eslint = require("gulp-eslint"),
+
+sass = require("gulp-sass"),
+cssnano = require("gulp-cssnano"),
+autoprefixer = require("gulp-autoprefixer");
+
 
 gulp.task("eslint", function (){
 	return gulp
 		.src("./js/*.js")
 		.pipe(eslint())
 		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
+		.pipe(eslint.failAfterError());
 })
+
+gulp.task('sass', function () {
+	return gulp
+		.src('./css/style.scss')
+		.pipe(sass())
+		.pipe(
+			autoprefixer({
+				browsers: ['last 2 versions'],
+			})
+		)
+		.pipe(gulp.dest('./build/css'))
+		.pipe(cssnano())
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('./build/css'))
+		.pipe(browserSync.reload({stream:true}));
+	});
 
 gulp.task("scripts", function(){
 		return gulp 
@@ -33,7 +55,9 @@ gulp.task("watch", function(){
 		}
 	})
 	gulp.watch("./js/*.js", gulp.series(["scripts", "eslint"]))
+	gulp.watch('./css/*.scss', gulp.series("sass"))
 	gulp.watch("./*html").on("change", browserSync.reload)
+
 });
 
 gulp.task("default", gulp.parallel("watch"));

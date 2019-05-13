e96b24d171d7180e748bdfa3dc98e890b2863fa9
jonathandsilva2 <jonathandsilva2@gmail.com>
2019-05-12 20:34:33 -0400

    added SASS variables and mixins

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    // $('select').selectric(); *omitted selectric menu selector due to bugs, to be added later
-
     $('select').on('change', function () {
         let section = $('select option:selected').val();
 
@@ -19,33 +17,28 @@ $(document).ready(function () {
         })
 
             .done(function (data) {
-
-
                 $('article').html('');
-
                 let articles = [];
 
                 for (let i = 0; data.results.length - 1; i++) { // creates an array of up to 12 articles that contain an image
                     if (data.results[i].multimedia[4] !== undefined) {
                         articles.push(data.results[i])
-                    } else if (data.results[i].multimedia[4] === undefined) {''
+                    } else if (data.results[i].multimedia[4] === undefined) {
+                    ' ' //do nothing
                     }
                     if (articles.length === 12) {
                         break;
                     }
-
                 }
-               
-
-
+            
                 setTimeout(function () {  // sets a delay towards displaying the articles to allow a loading gif to play.
-                    $('.loader').html('')
+                    
+                    $('.loader').append('<img src="assets/images/ajax-loader.gif" alt="loading">')
 
                     for (let i = 0; i <= 11; i++) {  //loops through the articles array and appends all relavant information on the webpage
                         let abstract = (articles[i].abstract)
                         let image = (articles[i].multimedia[4].url)
                         let link = (articles[i].short_url)
-
 
                         $('article').append('<a href = "' + link + '" class ="box' + i + ' media"><p>' + abstract + '</p></a>')
                         $(`.box${i}`).css('background-image', 'url("' + image + '")')
@@ -57,13 +50,8 @@ $(document).ready(function () {
                         $('a').css('color', 'white')
                         $(`.box${i} p`).css('background-color', 'rgba(0, 0, 0, 0.5)')
                         $(`.box${i} p`).css('margin-bottom', '0')
-
-
-
                     }
                 }, 200)
-
-
             })
 
             .fail(function () {
